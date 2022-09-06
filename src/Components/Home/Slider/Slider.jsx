0ff2c60e78c5ceb92fbae873cf5347ea0c673e2d
OmarCypha700 <98944012+OmarCypha700@users.noreply.github.com>
2022-09-05 18:52:42 -0700

    update Slider component

--- src/Components/Home/Slider/Slider.jsx
+++ src/Components/Home/Slider/Slider.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import "./Slider.css"
 
@@ -7,31 +7,31 @@ import "./Slider.css"
 const Slider = () => {
 
       // function for auto slider
-      const Slider = () =>{
-        let counter = 1;
-          setInterval(()=>{
-            document.getElementById("radio" + counter).checked = true;
-              counter++;
-              if(counter > 4){
-                  counter = 1;
-              }
-          },5000)
-      } 
+    //   const Slider = () =>{
+    //     let counter = 1;
+    //       setInterval(()=>{
+    //         document.getElementById("radio" + counter).checked = true;
+    //           counter++;
+    //           if(counter > 4){
+    //               counter = 1;
+    //           }
+    //       },5000)
+    //   } 
       
-    useEffect(()=>{
-        Slider();
-    }, [])
+    // useEffect(()=>{
+    //     Slider();
+    // }, [])
 
   return (
 
    <section className ="slider__body">
    <div className='slider'>
-       <div className="slides">
+       {/* <div className="slides"> */}
 
-   <input type="radio" name='radio__btn' id='radio1' />
-   <input type="radio" name='radio__btn' id='radio2' />
-   <input type="radio" name='radio__btn' id='radio3' />
-   <input type="radio" name='radio__btn' id='radio4' />
+   {/* <input type="radio" name='radio__btn' id='radio1' /> */}
+   {/* <input type="radio" name='radio__btn' id='radio2' /> */}
+   {/* <input type="radio" name='radio__btn' id='radio3' /> */}
+   {/* <input type="radio" name='radio__btn' id='radio4' /> */}
 
    {/* slide images start */}
 
@@ -42,7 +42,7 @@ const Slider = () => {
        <Link to="/shop"><button>Shop Now</button></Link>
    </div>
 
-   <div id='second' className="slide first">
+   {/* <div id='second' className="slide first">
        <h2>Get sleek sneakers</h2>
        <h1>At the chepest prices</h1>
        <h4>Save money while wearing quality</h4>
@@ -66,14 +66,14 @@ const Slider = () => {
    
        </div>
    {/* manual slider start */}
-   <div className="nav__manual">
+   {/* <div className="nav__manual">
        <label htmlFor="radio1" className='manual__btn'></label>
        <label htmlFor="radio2" className='manual__btn'></label>
        <label htmlFor="radio3" className='manual__btn'></label>
        <label htmlFor="radio4" className='manual__btn'></label>
-   </div>
+   </div> */}
    {/* manual slider end */}
-   </div>
+   {/* </div> */}
    {/* image slider end */}
     </section>
   )
